ffc27f20722842b1f26a8e28e2540dc25a0ca15e
Christopher Copeland <cmc232@zips.uakron.edu>
2021-09-09 00:57:05 -0400

    Added json response to fetch_blog route

--- src/routes/fetch_blog.rs
+++ src/routes/fetch_blog.rs
@@ -1,10 +1,25 @@
-use actix_web:: {HttpRequest, HttpResponse};
-use serde::*;
-//use actix_web::dev::HttpResponseBuilder;
+use actix_web:: {HttpRequest, HttpResponse, web};
 
-pub fn query_titles(req: HttpRequest) -> HttpResponse {
+#[derive(serde::Serialize)]
+struct BlogResponse {
+	name: String,
+	author: String,
+	rating: u8,
+}
+
+#[derive(serde::Deserialize)]
+pub struct BlogRequest {
+	id: u8
+}
+
+pub fn query_titles(req: web::Form<BlogRequest>) -> HttpResponse {
+	println!("id:{}",req.id);
+	let json = BlogResponse{ 
+			name: String::from("First Blog"),
+			author: String::from("Chris Copeland"),
+			rating: 5,
+		};
 	let response = HttpResponse::Ok()
-		.body("SENIP");
-
+		.json(json);
 	response
 }
--- src/startup.rs
+++ src/startup.rs
@@ -2,14 +2,14 @@ use actix_web::dev::Server;
 use actix_web::{App, web, HttpServer, };
 use std::net::TcpListener;
 
-use crate::routes::{ health_check, fetch_blog};
+use crate::routes::health_check;
 use crate::routes::fetch_blog::query_titles;
 
 pub fn run_it(listener: TcpListener) -> Result<Server, std::io::Error> {
 	let server = HttpServer::new(|| {
 		App::new()
 			.route( "/health_check", web::get().to(health_check))
-			.route( "/query_titles", web::get().to(query_titles))
+			.route( "/query_title", web::post().to(query_title))
 		
 	})
 	.listen(listener)?
